80bd27791171a600ad4b4dadc16b1d5d9ff949e4
Dmit1812 <dmit1812@nohost.org>
2023-10-27 08:19:38 +0000

    added lrucache to codebase

---
+++ pkg/lrucache/dllist.go
@@ -0,0 +1,164 @@
+package lrucache
+
+import "sync"
+
+type DLList interface {
+	Len() int
+	Front() *DLListItem
+	Back() *DLListItem
+	PushFront(v interface{}) *DLListItem
+	PushBack(v interface{}) *DLListItem
+	Remove(i *DLListItem)
+	MoveToFront(i *DLListItem)
+}
+
+type DLListItem struct {
+	Value interface{}
+	Next  *DLListItem
+	Prev  *DLListItem
+}
+
+type dllist struct {
+	len   int
+	mu    sync.Mutex
+	front *DLListItem
+	back  *DLListItem
+}
+
+// Len returns the length of the dllist.
+func (l *dllist) Len() int {
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.len
+}
+
+// Front returns address of the front DLListItem of the dllist.
+func (l *dllist) Front() *DLListItem {
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	return l.front
+}
+
+// Back returns address of the back DLListItem of the dllist.
+func (l *dllist) Back() *DLListItem {
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	return l.back
+}
+
+// pushFront private method adds an already created DLListItem to the front of the dllist
+// BEWARE! it assumes that the mutex lock should be set by the caller
+func (l *dllist) pushFront(i *DLListItem) *DLListItem {
+	// exit if i is nil
+	if i == nil {
+		return nil
+	}
+
+	// initialize the new front of the dllist
+	i.Prev = nil
+	i.Next = l.front
+
+	// if our front is initialized with pointer set old front to point to i (new front)
+	if l.front != nil {
+		l.front.Prev = i
+	} else {
+		// if our front is nil then it's the first element and both front and back should point to it
+		l.back = i
+	}
+
+	// set front to the i
+	l.front = i
+
+	// increase array size
+	l.len++
+	return i
+}
+
+// PushFront adds a new value to the back of the dllist.
+func (l *dllist) PushFront(v interface{}) *DLListItem {
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	return l.pushFront(&DLListItem{Value: v, Next: nil, Prev: nil})
+}
+
+// PushBack adds a new value to the back of the dllist.
+func (l *dllist) PushBack(v interface{}) *DLListItem {
+	// critical section start
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	// create a new DLListItem with value from v
+	newback := &DLListItem{Value: v, Next: nil, Prev: l.back}
+
+	// if out back is initilized then make sure that previous element points to new back element
+	if l.back != nil {
+		l.back.Next = newback
+	} else {
+		// if our back is nil then newback is the first element and both front and back should point to it
+		l.front = newback
+	}
+	// set back to the newback element
+	l.back = newback
+	// increase array size
+	l.len++
+	return newback
+}
+
+// remove removes a DLListItem from the dllist
+// no checking if the item is in the dllist performed
+// BEWARE! it assumes that the mutex lock should be set by the caller
+func (l *dllist) remove(i *DLListItem) {
+	// exit if i is nil
+	if i == nil {
+		return
+	}
+
+	// if previous element of element i is defined, then make sure that it points to the element that is after alement i
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		// there were no previous element, so we are removing the first element. Next element to current becomes new first
+		l.front = i.Next
+	}
+	// if next element of element i is defined, then make sure that it points back to the element that is before i
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		// there was no next element, so we are removing the last element. Previous element to current becomes new last
+		l.back = i.Prev
+	}
+	// we removed an element from the dllist reduce array size
+	l.len--
+}
+
+// Remove will set mutex lock and call the remove to remove a DLListItem from the dllist
+func (l *dllist) Remove(i *DLListItem) {
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.remove(i)
+}
+
+// MoveToFront moves a DLListItem to the front of the dllist
+// no checking if the item is from the the dllist performed
+func (l *dllist) MoveToFront(i *DLListItem) {
+	// critical section start, it ends when return is called
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.remove(i)
+	l.pushFront(i)
+}
+
+// NewDLList creates a new dllist
+func NewDLList() DLList {
+	return new(dllist)
+}
---
+++ pkg/lrucache/lrucache_test.go
@@ -0,0 +1,151 @@
+package lrucache
+
+import (
+	"math/rand"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCache(t *testing.T) {
+	t.Run("empty cache, uses NewCacheWithOnDelete", func(t *testing.T) {
+		c := NewCacheWithOnDelete(10, nil)
+
+		_, ok := c.Get("aaa")
+		require.False(t, ok)
+
+		_, ok = c.Get("bbb")
+		require.False(t, ok)
+	})
+
+	t.Run("simple", func(t *testing.T) {
+		c := NewCache(5)
+
+		wasInCache := c.Set("aaa", 100)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200)
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 100, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		wasInCache = c.Set("aaa", 300)
+		require.True(t, wasInCache)
+
+		val, ok = c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+
+		val, ok = c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+	t.Run("clear shall work and run 3 deletions", func(t *testing.T) {
+		var a int32
+
+		myOnDeleteFunc := func(key Key, value interface{}) {
+			s := key
+			v := value.(int)
+			atomic.AddInt32(&a, 1)
+			_ = s
+			_ = v
+		}
+
+		c := NewCacheWithOnDelete(5, myOnDeleteFunc)
+
+		_ = c.Set("aaa", 100)
+		_ = c.Set("bbb", 200)
+		_ = c.Set("ddd", 300)
+
+		c.Clear()
+
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+		require.Equal(t, int32(3), atomic.LoadInt32(&a))
+	})
+
+	t.Run("purge logic - extra element out of 3 shall be purged", func(t *testing.T) {
+		c := NewCache(3)
+
+		_ = c.Set("aaa", 100)
+		_ = c.Set("bbb", 200)
+		_ = c.Set("ccc", 300)
+		_ = c.Set("ddd", 400)
+
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		val, ok = c.Get("ddd")
+		require.True(t, ok)
+		require.Equal(t, 400, val)
+	})
+
+	t.Run("purge logic - the least used element out of 3 shall be purged", func(t *testing.T) {
+
+		c := NewCache(3)
+
+		_ = c.Set("aaa", 100)
+		_ = c.Set("bbb", 200)
+		_ = c.Set("ccc", 300)
+		_, _ = c.Get("aaa")
+		_, _ = c.Get("aaa")
+		_, _ = c.Get("bbb")
+		_, _ = c.Get("bbb")
+
+		_ = c.Set("ddd", 400)
+
+		val, ok := c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+}
+
+func TestCacheMultithreading(t *testing.T) {
+	//t.Skip() // Remove me if task with asterisk completed.
+
+	var a int32
+
+	myOnDeleteFunc := func(key Key, value interface{}) {
+		s := key
+		v := value.(int)
+		atomic.AddInt32(&a, 1)
+		_ = s
+		_ = v
+	}
+
+	c := NewCacheWithOnDelete(200, myOnDeleteFunc)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 20_000; i++ {
+			c.Set(Key(strconv.Itoa(i)), i)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 20_000; i++ {
+			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+		}
+	}()
+
+	wg.Wait()
+
+	require.Equal(t, int32(19_800), atomic.LoadInt32(&a), "number of deletions from cache should be 999000, but it was not")
+}
